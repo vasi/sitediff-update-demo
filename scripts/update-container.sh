2ddb9fbfc615898b060f1651b743e784286d48d2
Dave Vasilevsky <dave@vasilevsky.ca>
2015-04-15 18:33:54 +0000

    Expand Add Content menu item

--- scripts/update-container.sh
+++ scripts/update-container.sh
@@ -4,3 +4,9 @@ drush dis -y comment
 drush en -y features
 drush en -y node_bug_7_36 permission_create_bug_7_36
 
+# Expand the "Add Content" menu item
+drush scr - <<'EOS'
+  $link = menu_link_get_preferred("node/add", "navigation");
+  $link["expanded"] = 1;
+  menu_link_save($link);
+EOS
