46dbdb2aebf4059f69c094f2eb2620a5f36d0e51
Dave Vasilevsky <dave@vasilevsky.ca>
2015-04-15 19:13:53 +0000

    Forget about expanding links

--- sites/all/scripts/update-container.sh
+++ sites/all/scripts/update-container.sh
@@ -6,10 +6,3 @@ drush en -y node_bug_7_36 permission_create_bug_7_36
 
 # Remove the content-type feature, but leaving the content-type intact (7.35)
 drush dis -y node_bug_7_36
-
-# Expand the "Add Content" menu item
-drush scr - <<'EOS'
-  $link = menu_link_get_preferred("node/add", "navigation");
-  $link["expanded"] = 1;
-  menu_link_save($link);
-EOS
