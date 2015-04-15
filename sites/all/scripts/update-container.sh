59d892e6b5f69ab94860b36756d691e90a13d56e
Dave Vasilevsky <dave@vasilevsky.ca>
2015-04-15 20:05:54 +0000

    Add more create permissions

--- sites/all/scripts/update-container.sh
+++ sites/all/scripts/update-container.sh
@@ -2,7 +2,7 @@
 
 drush dis -y comment
 drush en -y features
-drush en -y node_bug_7_36 permission_create_bug_7_36
+drush en -y node_bug_7_36 permission_create
 
 # Remove the content-type feature, but leaving the content-type intact (7.35)
 drush dis -y node_bug_7_36
