f5d83301da465abc20bccc3c172e9fecf3bd3954
Dave Vasilevsky <dave@vasilevsky.ca>
2015-04-15 19:03:16 +0000

    Remove content-type feature

--- sites/all/scripts/update-container.sh
+++ sites/all/scripts/update-container.sh
@@ -4,6 +4,9 @@ drush dis -y comment
 drush en -y features
 drush en -y node_bug_7_36 permission_create_bug_7_36
 
+# Remove the content-type feature, but leaving the content-type intact (7.35)
+drush dis -y node_bug_7_36
+
 # Expand the "Add Content" menu item
 drush scr - <<'EOS'
   $link = menu_link_get_preferred("node/add", "navigation");
