a024216a75d43f83d87f618c1c6ffa0e3efe1d6e
Dave Vasilevsky <dave@vasilevsky.ca>
2015-05-12 17:34:26 -0700

    Rename content type

--- sites/all/scripts/update-container.sh
+++ sites/all/scripts/update-container.sh
@@ -2,7 +2,7 @@
 
 drush dis -y comment
 drush en -y features
-drush en -y node_bug_7_36 permission_create
+drush en -y node_lorem permission_create
 
 # Remove the content-type feature, but leaving the content-type intact (7.35)
-drush dis -y node_bug_7_36
+drush dis -y node_lorem
